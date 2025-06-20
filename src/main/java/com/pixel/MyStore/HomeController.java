3d3dd32e6f84a7bd3be8f2478e6e77dc4079954c
Tatiana Ceban <tatiana.ceban.19@bk.ru>
2025-06-20 15:18:30 +0300

    feat: Add my first list

---
+++ src/main/java/com/pixel/MyStore/HomeController.java
@@ -0,0 +1,25 @@
+package com.pixel.MyStore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"", "/"})
+    public String index() {
+        return "index";
+    }
+
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/privacy")
+    public String privacy() {
+        return "privacy";
+    }
+
+}
